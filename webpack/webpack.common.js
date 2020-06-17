c3ff74afcccab1398934d1537c3a0dca3eba3f65
Olga Boris <olgaboris94@gmail.com>
2020-06-17 17:05:45 +0300

    initial dev build

---
+++ webpack/webpack.common.js
@@ -0,0 +1,116 @@
+const { join } = require("path");
+
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const ScriptExtensionPlugin = require("script-ext-html-webpack-plugin");
+// const StylelintPlugin = require("stylelint-webpack-plugin");
+
+const PATH_DIST = join(__dirname, "../dist");
+
+const CONFIG = {
+    entry: {
+        index: "./index.js",
+    },
+
+    output: {
+        path: PATH_DIST,
+        filename: "[name].[hash:8].bundle.js",
+        chunkFilename: "[name].[hash:8].chunk.js",
+    },
+
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".scss", ".svg"],
+    },
+
+    optimization: {
+        splitChunks: {
+            name: true,
+            chunks: "all",
+            cacheGroups: {
+                vendor: {
+                    name: "vendor",
+                    test: /[\\]node_modules[\\/]/,
+                },
+            },
+        },
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html",
+            inject: "body",
+            filename: "index.html",
+        }),
+        // new StylelintPlugin({
+        //     configFile: ".stylelintrc",
+        // }),
+        // new MiniCssExtractPlugin({
+        //     filename: "[name].css",
+        // }),
+        new CleanWebpackPlugin({
+            verbose: true,
+        }),
+        // new ScriptExtensionPlugin({
+        //     defaultAttribute: "defer",
+        // }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    // {
+                    //     loader: MiniCssExtractPlugin.loader,
+                    //     options: {
+                    //         hmr: process.env.NODE_ENV === "development",
+                    //     },
+                    // },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.ts(x?)|.js|.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    "ts-loader",
+                    // {
+                    //     loader: "eslint-loader",
+                    //     options: {
+                    //         emitWarning: true,
+                    //     },
+                    // },
+                ],
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: "svg-url-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.(otf)|.(ttf)|.(png)$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+};
+
+module.exports = {
+    CONFIG,
+    PATH_DIST,
+};
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,25 @@
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+
+const { CONFIG, PATH_DIST } = require("./webpack.common.js");
+
+const HOST = "localhost";
+const PORT = 3000;
+
+module.exports = (env) =>
+    merge(CONFIG, {
+        mode: "development",
+        devtool: "cheap-module-source-map",
+
+        plugins: [new webpack.HotModuleReplacementPlugin()],
+
+        devServer: {
+            port: PORT,
+            host: HOST,
+            hot: true,
+            compress: true,
+            historyApiFallback: true,
+            contentBase: PATH_DIST,
+            watchContentBase: true,
+        },
+    });
